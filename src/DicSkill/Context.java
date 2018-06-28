382d23385efc9db90929cf3099447f5dfcdf4ba7
Lia Frischholz <pharmafairyy@gmail.com>
2018-06-28 14:35:14 +0200

    Update 28.06.28
    code comments were proof read.

--- src/DicSkill/Context.java
+++ src/DicSkill/Context.java
@@ -6,6 +6,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * 28.06.2018
+ * NEW:
+ * -	improved code documentation
+ *	@author Lia, Walter
+ */
+
+/**
  * 27.06.2018
  * NEW:
  * -	changed how changing pref categories works.
@@ -24,7 +31,6 @@ import java.util.regex.Pattern;
  * NEW:
  * -	Context is now savable
  * @author Lia
- *
  */
 
 /**
@@ -35,7 +41,6 @@ import java.util.regex.Pattern;
  * TO DO:
  * -	make context saveable
  * @author Lia
- *
  */
 
 /**
@@ -43,14 +48,13 @@ import java.util.regex.Pattern;
  * TO DO:
  * -	make context saveable
  * @author Lia
- *
  */
 
 /**
  * Context remembers the important information of the last user queries.
- * This includes the wished word, category and function. The information is used to improve the answer for the user.
+ * This includes the last wished word, preferred categories and the last function. The information is used to improve the answer to the user.
  *
- * Context is will be persistently saved through State.
+ * Context is persistently saved through State in class MessageManager
  */
 public class Context implements Serializable {
 
@@ -61,26 +65,38 @@ public class Context implements Serializable {
 	private ArrayList<String> preferredCategory;
 	private String lastWishedWord;
 
-	/** Constructor **/
+	/** CONSTRUCTOR **/
 	public Context(){
 		preferredCategory = new ArrayList<String>();
 	}
 	
-	/** Methods **/
+	/** METHODS **/
 
 	/**
 	 * This function finds out what change the user wants to make to the preferred categories
-	 * by checking for keywords. Then the function which will make the change is called.
-	 * Categories are groups of words to distinguish between seemingly same words.
-	 * They can be added or removed.
+	 * by checking for keywords using regex. Then the function performing the change is called.
+	 * Categories are words, which will be used to put results which probably match the category first.
+	 * 
+	 * They can be added, removed or all deleted. If this function cannot find which of the three changes the user wants 
+	 * to make, it will tell the user which changes he/she can make.
 	 *
 	 * @param msg from the user
 	 */
 	public void changePrefCat(String msg) {
 		
+		/*
+		 * Regex explained for those new to regex: "^.*(add|remove|delete all)([ a-zA-Z][a-zA-Z]*)( to | from )?(my |the )?(preferred category|preferred categories).*$"
+		 * ^.* means the string may start with any sequence of character or no characters
+		 * (add|remove|delete all) means one of the options must follow
+		 * ([ a-zA-Z][a-zA-Z]*) means any alphabetical letter may follow, but only the first character may be a space
+		 * ( to | from )? AND (my |the )? means one of the options may follow, but does not have to
+		 * (preferred category|preferred categories) means one of the options must follow
+		 * .*$ means the string may end with any character, no matter how many.
+		 */
 		Pattern pattern = Pattern.compile("^.*(add|remove|delete all)([ a-zA-Z][a-zA-Z]*)( to | from )?(my |the )?(preferred category|preferred categories).*$");
 		Matcher matcher = pattern.matcher(msg);
 		
+		// note: matcher.group(2) returns the string of the third group. A group is marked by (). If there are more than one option, it will return the option used in the msg.
 		if(matcher.matches()) {
 			
 			if(matcher.group(1).matches("add")) {
@@ -108,17 +124,23 @@ public class Context implements Serializable {
 	}
 
 	/**
-	 * A category will be added as by the users request. If none was found the user will be notified.
+	 * A category the user names will be added to preferred categories. If the category was already added,
+	 * it will not add it again and tell the user.
 	 *
 	 * @param cat category the user wants to add
 	 */
 	private void addPrefCat(String cat) {
-		preferredCategory.add(cat);
-		System.out.println(cat + " was added to your preferred categories.");
+		if(preferredCategory.contains(cat)) {
+			preferredCategory.add(cat);
+			System.out.println(cat + " was added to your preferred categories.");
+		}
+		else {
+			System.out.println(cat + " is already in your preferred categories.");
+		}
 	}
 
 	/**
-	 * A category will be removed as by the users request. If none was found the user will be notified.
+	 * The category the user names will be removed. If none was found the user will be notified.
 	 *
 	 * @param cat category the user wants to remove
 	 */
@@ -133,39 +155,34 @@ public class Context implements Serializable {
 	}
 
 	/**
-	 * All categories will be removed.
+	 * All categories will be removed from preferred categories.
 	 */
 	private void deleteAllPrefCat() {
 		preferredCategory.clear();
 		System.out.println("All your preferred categories were deleted.");
 	}
 	
-	/** Setters and Getters **/
+	/** SETTERS AND GETTERS **/
 
 	public Function getLastFunctionUsed() {
 		return lastFunctionUsed;
 	}
 
-
 	public void setLastFunctionUsed(Function lastFunctionUsed) {
 		this.lastFunctionUsed = lastFunctionUsed;
 	}
-
 
 	public ArrayList<String> getPreferredCategory() {
 		return preferredCategory;
 	}
 
-
 	public void setPreferredCategory(ArrayList<String> preferredCategory) {
 		this.preferredCategory = preferredCategory;
 	}
 
-
 	public String getLastWishedWord() {
 		return lastWishedWord;
 	}
-
 
 	public void setLastWishedWord(String lastWishedWord) {
 		this.lastWishedWord = lastWishedWord;
